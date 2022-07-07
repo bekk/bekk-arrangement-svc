321887676adee074892814459fc19ecb2eb8c671
Bjørn-Ivar Strøm <bjornivar.strom@gmail.com>
2022-07-07 15:48:59 +0200

    remove /api/ from health endpoint

--- Arrangement-Svc/Handlers/Health.fs
+++ Arrangement-Svc/Handlers/Health.fs
@@ -3,4 +3,4 @@ module Health
 open Giraffe
 
 let healthCheck: HttpHandler =
-    route "/api/health" >=> Successful.OK "Health check: dette gikk fint"
+    route "/health" >=> Successful.OK "Health check: dette gikk fint"
