5261d457a3c9038b388e4d478431e916310cb88c
Bjørn-Ivar Strøm <bjornivar.strom@gmail.com>
2022-07-07 14:54:33 +0200

    Remove authentication from index.ts

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -4,24 +4,19 @@ import { App } from 'src/components/App/App';
 import 'src/extension-methods/array';
 import 'src/index.css';
 import { NotificationHandler } from './components/NotificationHandler/NotificationHandler';
-import {authenticateUser, isAuthenticated} from "src/auth";
 import {getConfig, setConfig} from "src/config";
 
 const init = async () => {
   const config = await getConfig()
   setConfig(config)
-  if (!isAuthenticated()) {
-    authenticateUser();
-  } else {
-    const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-    root.render (
-      <React.StrictMode>
-        <NotificationHandler>
-          <App />
-        </NotificationHandler>
-      </React.StrictMode>
-    )
-  }
+  const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+  root.render (
+    <React.StrictMode>
+      <NotificationHandler>
+        <App />
+      </NotificationHandler>
+    </React.StrictMode>
+  )
 }
 
 init()
