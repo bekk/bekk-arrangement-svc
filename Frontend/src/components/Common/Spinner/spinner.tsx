720a87505a545e6eabcaf0dda75d5faa28a25f4e
Bjørn-Ivar Strøm <bjornivar.strom@gmail.com>
2022-09-23 09:12:13 +0200

    Unstyled spinner when loading events
    Using the spinner made event cards really long and look weird.
    This change uses an unstyled spinner instead to stop making the DOM jump around.

--- Frontend/src/components/Common/Spinner/spinner.tsx
+++ Frontend/src/components/Common/Spinner/spinner.tsx
@@ -5,6 +5,9 @@ export const Spinner = () => {
   return (
     <div className={style.spinnerContainer}>
       <div className={style.spinner}/>
-    </div>
-)
+    </div>)
+}
+
+export const UnstyledSpinner = () => {
+  return <div className={style.spinner}/>
 }
