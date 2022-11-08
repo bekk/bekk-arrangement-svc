64a61c7b0ce7eca877e17ffd9d02b89be988dfaf
Bjørn-Ivar Strøm <bjornivar.strom@gmail.com>
2022-11-08 09:20:36 +0100

    wip

--- TestProject1/Program.fs
+++ TestProject1/Program.fs
@@ -1,1 +1,7 @@
-module Program = let [<EntryPoint>] main _ = 0
+open System
+
+module Program =
+    let [<EntryPoint>] main _ =
+        Environment.SetEnvironmentVariable("DOCKER_HOST", "unix:///tmp/podman.sock")
+        Environment.SetEnvironmentVariable("TESTCONTAINERS_RYUK_DISABLED", "true")
+        0
--- TestProject1/Tests.fs
+++ TestProject1/Tests.fs
@@ -1,23 +1,29 @@
-module Tests
+namespace Tests
 
 open System
 open Xunit
-open Xunit.Abstractions
+open FsUnit.Xunit
 open Microsoft.AspNetCore.Mvc.Testing
+open Microsoft.Extensions.Configuration
 
-type Program = class end
-
-type Foo(factory: WebApplicationFactory<Program>) =
-    interface IClassFixture<WebApplicationFactory<Program>>
-
-type BasicFixture() =
+type DatabaseFixture() =
+    let factory = new WebApplicationFactory<App.Program>()
+    let config = factory.Services.GetService(typeof<IConfiguration>) :?> IConfiguration
     interface IDisposable with
-        member this.Dispose () =
-            ()
+        member this.Dispose () = factory.Dispose()
 
+[<CollectionDefinition("Database collection")>]
+type DatabaseCollection() =
+    interface ICollectionFixture<DatabaseFixture>
+
+[<Collection("Database collection")>]
 type Tests() =
     [<Fact>]
-    let ``My test`` () =
-        Assert.True(true)
+    member _.``My test`` () =
+        true |> should be True
 
-    interface IClassFixture<Foo> with
+[<Collection("Database collection")>]
+type Tests2() =
+    [<Fact>]
+    member _.``My test 2`` () =
+        true |> should be True
