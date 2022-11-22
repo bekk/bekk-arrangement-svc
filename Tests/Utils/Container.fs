3445187beb37709a186a996bc5353443de6f85ef
Bjørn-Ivar Strøm <bjornivar.strom@gmail.com>
2022-11-22 08:32:39 +0100

    Remove printfn

--- Tests/Utils/Container.fs
+++ Tests/Utils/Container.fs
@@ -34,20 +34,15 @@ let startContainer () =
     ()
 
 let containerExists () =
-    printfn "Container exists?"
     let result =
         cli {
             Exec ContainerManagerProgram
             Arguments $"""ps --all --filter name={ContainerName} --format "{{.ID}}" """
         }
         |> Command.execute
-    printfn "Result is: %A" result
     match result.Text with
     | None -> false
-    | Some text ->
-        printfn "Text is: %A" text
-        printfn "Is it empty? %A" (text <> "")
-        text <> ""
+    | Some text -> text <> ""
 
 let containerIsStopped() =
     let result =
