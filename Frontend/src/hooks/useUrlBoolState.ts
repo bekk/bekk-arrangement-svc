f547fc71bc1cab13eb351d794b539ac5fea6c21c
Bjørn-Ivar Strøm <bjorn.ivar.strom@bekk.no>
2023-02-14 10:06:34 +0100

    Add url bool state hook

---
+++ Frontend/src/hooks/useUrlBoolState.ts
@@ -0,0 +1,30 @@
+import {useHistory} from "react-router";
+import {useEffect, useState} from "react";
+
+export const useUrlBoolState = (initialValue: boolean, key: string) => {
+    const serializeBool = (bool: boolean) => bool ? "true" : "false"
+    const deserializeBool = (string: string) => string === "true"
+    const history = useHistory();
+    const search = new URLSearchParams(history.location.search);
+
+    const urlValue = search.get(key);
+    const [state, setCurrentState] = useState(urlValue ? urlValue : serializeBool(initialValue));
+
+    const setState = (state: boolean) => {
+        setCurrentState(serializeBool(state));
+        const searchParameters = new URLSearchParams(history.location.search);
+        searchParameters.set(key, serializeBool(state));
+        const pathname = history.location.pathname;
+        history.push({ pathname, search: searchParameters.toString()})
+    }
+
+    useEffect(() => {
+        if (urlValue && urlValue !== state)
+            setState(deserializeBool(urlValue))
+        if (!urlValue && initialValue)
+            setState(initialValue)
+    }, [urlValue])
+
+
+    return [deserializeBool(state), setState] as const;
+}
