1b8497dd655d2fc969b841170ca1960321d32609
Bjørn-Ivar Strøm <bjorn.ivar.strom@bekk.no>
2023-02-22 14:07:31 +0100

    Tests for query is external

--- Tests/Query.fs
+++ Tests/Query.fs
@@ -8,6 +8,36 @@ type Queries(fixture: DatabaseFixture) =
     let authenticatedClient =
         fixture.getAuthedClient
         
+        
+    [<Fact>]
+    member _.``Check if event is external returns true if event is external``() =
+        let event =
+            TestData.createEvent (fun e -> { e with IsExternal = true })
+        task {
+            let! createdEvent = Helpers.createEventAndGet authenticatedClient event
+            let eventId = System.Guid.Parse createdEvent.Event.Id
+            let! result = Queries.isEventExternal eventId fixture.dbContext
+
+            match result with
+            | Ok isExternal -> Assert.True(isExternal)
+            | Error e -> failwith $"Test failed: {e}"
+        }
+
+    [<Fact>]
+    member _.``Check if event is external returns false if event is not external``() =
+        let event =
+            TestData.createEvent (fun e -> { e with IsExternal = false })
+        task {
+            let! createdEvent = Helpers.createEventAndGet authenticatedClient event
+            let eventId = System.Guid.Parse createdEvent.Event.Id
+            let! result = Queries.isEventExternal eventId fixture.dbContext
+
+            match result with
+            | Ok isExternal -> Assert.False(isExternal)
+            | Error e -> failwith $"Test failed: {e}"
+        }
+
+        
     [<Fact>]
     member _.``Querying non-existing participant gives no result``() =
         task {
