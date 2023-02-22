e13e6478465e13d1b5ec42fbb79dfdf95eba31aa
Bjørn-Ivar Strøm <bjorn.ivar.strom@bekk.no>
2023-02-22 14:27:12 +0100

    Add test for Queries.canEditEvent

--- Tests/Query.fs
+++ Tests/Query.fs
@@ -8,7 +8,6 @@ type Queries(fixture: DatabaseFixture) =
     let authenticatedClient =
         fixture.getAuthedClient
         
-        
     [<Fact>]
     member _.``Check if event is external returns true if event is external``() =
         let event =
@@ -36,8 +35,61 @@ type Queries(fixture: DatabaseFixture) =
             | Ok isExternal -> Assert.False(isExternal)
             | Error e -> failwith $"Test failed: {e}"
         }
-
         
+    [<Fact>]
+    member _.``Check if admin can edit event returns true when isAdmin = true``() =
+        let event = TestData.createEvent id
+        task {
+            let! createdEvent = Helpers.createEventAndGet authenticatedClient event
+            let eventId = System.Guid.Parse createdEvent.Event.Id
+            let! result = Queries.canEditEvent eventId true None "" fixture.dbContext
+
+            match result with
+            | Ok canEdit -> Assert.True(canEdit)
+            | Error e -> failwith $"Test failed: {e}"
+        }
+    [<Fact>]
+    member _.``Check if organizer can edit event returns true when OrganizerId matches employeeId``() =
+        let event = TestData.createEvent id
+        task {
+            let! createdEvent = Helpers.createEventAndGet authenticatedClient event
+            let eventId = System.Guid.Parse createdEvent.Event.Id
+            let employeeId = createdEvent.Event.OrganizerId
+            let! result = Queries.canEditEvent eventId false (Some employeeId) System.Guid.Empty fixture.dbContext
+    
+            match result with
+            | Ok canEdit -> Assert.True(canEdit)
+            | Error e -> failwith $"Test failed: {e}"
+        }
+        
+    [<Fact>]
+    member _.``Check if edit token can edit event returns true when EditToken matches``() =
+        let event =
+            TestData.createEvent id
+        task {
+            let! createdEvent = Helpers.createEventAndGet authenticatedClient event
+            let eventId = System.Guid.Parse createdEvent.Event.Id
+            let editToken = createdEvent.EditToken
+            let! result = Queries.canEditEvent eventId false None editToken fixture.dbContext
+    
+            match result with
+            | Ok canEdit -> Assert.True(canEdit)
+            | Error e -> failwith $"Test failed: {e}"
+        }
+    
+    [<Fact>]
+    member _.``Check if non-admin, non-organizer, and non-token holder cannot edit event``() =
+        let event = TestData.createEvent id
+        task {
+            let! createdEvent = Helpers.createEventAndGet authenticatedClient event
+            let eventId = System.Guid.Parse createdEvent.Event.Id
+            let! result = Queries.canEditEvent eventId false None System.Guid.Empty fixture.dbContext
+    
+            match result with
+            | Ok canEdit -> Assert.False(canEdit)
+            | Error e -> failwith $"Test failed: {e}"
+        }
+    
     [<Fact>]
     member _.``Querying non-existing participant gives no result``() =
         task {
