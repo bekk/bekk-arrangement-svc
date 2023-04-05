2c19d328cbbc77e7cc8c8ff0130bb1b063157923
Bjørn-Ivar Strøm <bjorn.ivar.strom@bekk.no>
2023-04-05 09:19:45 +0200

    icons

--- Frontend/src/components/Common/Icons/OfficeIcon.tsx
+++ Frontend/src/components/Common/Icons/OfficeIcon.tsx
@@ -7,20 +7,16 @@ interface IconProps {
 
 export const OfficeIcon = ({ color, className }: IconProps) => {
   return (
-    <svg
-      width="21"
-      height="29"
-      viewBox="0 0 21 29"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      shapeRendering="crispEdges">
-      <title>Kontor</title>
-      <path fill="none" d="M19.5 2H1V28H19.5V2Z" stroke={color} />
-      <path fill="none" d="M9 6H5V10H9V6Z" stroke={color} />
-      <path fill="none" d="M9 13H5V17H9V13Z" stroke={color} />
-      <path fill="none" d="M16 6H12V10H16V6Z" stroke={color} />
-      <path fill="none" d="M16 13H12V17H16V13Z" stroke={color} />
-      <path fill="none" d="M13 20H8V28H13V20Z" stroke={color} />
-    </svg>
+      <svg width="21" height="24" viewBox="0 0 21 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+
+            className={className}>
+
+          <path d="M20 1H1V23H20V1Z" stroke={color} strokeWidth="0.5"/>
+          <path d="M9.5 4H6.5V8H9.5V4Z" stroke={color} strokeWidth="0.5"/>
+          <path d="M9.5 10H6.5V14H9.5V10Z" stroke={color} strokeWidth="0.5"/>
+          <path d="M15.5 4H12.5V8H15.5V4Z" stroke={color} strokeWidth="0.5"/>
+          <path d="M15.5 10H12.5V14H15.5V10Z" stroke={color} strokeWidth="0.5"/>
+          <path d="M12.5 16H8.5V23H12.5V16Z" stroke={color} strokeWidth="0.5"/>
+      </svg>
   );
 };
