6b6dc44099c683e57102566edc8e78d048083542
Bjørn-Ivar Strøm <bjorn.ivar.strom@bekk.no>
2023-07-26 08:56:28 +0200

    Define global for dev as well

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-    plugins: [react(),tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  let config = {
+    plugins: [react(), tsconfigPaths()],
     server: {
-        port: 3000,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:5000',
-                changeOrigin: true,
-            }
-        }
-    }
-})
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.PROXY || 'http://localhost:5000',
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+
+  // As this was ported from CRA using webpack to vite
+  // We need to define global.
+  // For dev environment we do it here.
+  // For prod environment we do it in vite-env.d.ts
+  if (mode === 'development') {
+      config = {...config, define: { global: "window"} }
+  }
+
+  return config;
+});
