9c8c4c6ae83d889d37f3d7c1feed1a08b4ba4cf0
Bjørn-Ivar Strøm <bjorn.ivar.strom@bekk.no>
2023-08-02 09:34:08 +0200

    Store cancel-event state in session state

--- Frontend/src/components/Common/ButtonWithConfirmModal/ButtonWithPromptModal.tsx
+++ Frontend/src/components/Common/ButtonWithConfirmModal/ButtonWithPromptModal.tsx
@@ -4,6 +4,9 @@ import { Modal } from 'src/components/Common/Modal/Modal';
 import { ReactChild } from 'src/types';
 import style from './ButtonWith.module.scss';
 import { TextArea } from 'src/components/Common/TextArea/TextArea';
+import { useSessionState } from 'src/hooks/sessionState';
+import { useParam } from 'src/utils/browser-state';
+import { eventIdKey } from 'src/routing';
 
 interface IProps {
   text: string;
@@ -22,8 +25,12 @@ export function ButtonWithPromptModal({
   textareaLabel,
   className,
 }: IProps) {
+  const eventId = useParam(eventIdKey);
   const [showModal, setShowModal] = useState(false);
-  const [promptAnswer, setPromptAnswer] = useState('');
+  const [promptAnswer, setPromptAnswer] = useSessionState(
+    '',
+    `cancel-${eventId}`
+  );
 
   const confirmAndClose = () => {
     onConfirm(promptAnswer);
