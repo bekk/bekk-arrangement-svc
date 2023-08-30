9e4f6cc034e740a0cc90d2106e6ba6bb0fe0ae20
Bjørn-Ivar Strøm <bjorn.ivar.strom@bekk.no>
2023-08-30 09:07:41 +0200

    Spike

--- Arrangement-Svc/LoadTests/Spike-RegisterParticipant.js
+++ Arrangement-Svc/LoadTests/Spike-RegisterParticipant.js
@@ -15,8 +15,8 @@ export let options = {
 
 export default function () {
   const token =
-    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik56RTVOVFJHUVRnNVJVRkNSVFJEUkRnelEwUXdORE5GUkRZeU4wWkZNVEJGUmpCRFFUVkJRUSJ9.eyJodHRwczovL2FwaS5iZWtrLm5vL2NsYWltcy9wZXJtaXNzaW9uIjpbInJlYWQ6c3RhZmZpbmciLCJ3cml0ZTpzdGFmZmluZyIsIndyaXRlOnRlbnRhdGl2U3RhZmZpbmciLCJyZWFkOnRlbnRhdGl2U3RhZmZpbmciLCJyZWFkOmVtcGxveWVlcyIsInJlYWQ6YmVrayIsIndyaXRlOmVtcGxveWVlcyIsIndyaXRlOnRpbWVjb2RlcyIsIndyaXRlOmFjY291bnRpbmciLCJhZG1pbjppbnZvaWNlLXN2YyIsImFkbWluOlNhbGVzQW5kUHJvamVjdHMtc3ZjIiwicmVhZDpvcHBvcnR1bml0eSIsIndyaXRlOm9wcG9ydHVuaXR5Iiwid3JpdGU6cHJvamVjdCIsInJlYWRXcml0ZTpwcm9nbm9zaXMiLCJyZWFkV3JpdGU6c3ViY29udHJhY3RvciIsImFkbWluOnRpbWVrZWVwZXItc3ZjIiwicmVhZDplZ2VubWVsZGluZ2VyIiwicmVhZDp0aW1lY29kZXMiLCJyZWFkV3JpdGU6dGltZXNoZWV0cyIsInJlYWQ6aW52b2ljZXMiLCJyZWFkOmN1c3RvbWVyIiwicmVhZDpwcm9qZWN0IiwicmVhZFdyaXRlOmNhbGVuZGFyIiwiYmF0Y2hVcGRhdGU6Y2FsZW5kYXIiLCJyZWFkV3JpdGU6cGFya2luZyIsInJlYWRXcml0ZTpldmVudHMiLCJ3cml0ZTpwcmFjdGljZUdyb3VwcyIsInJlYWQ6Y3YiLCJkZWxldGU6YXV0aDB1c2VyIiwiYWRtaW46Y2FiaW4iLCJyZWFkOmZvcnNpZGUiLCJyZWFkOmNhYmluIiwiYWRtaW46YXV0aCIsImFkbWluOmVtcGxveWVlLXN2YyIsImFkbWluOmFycmFuZ2VtZW50IiwicmVhZDphcnJhbmdlbWVudCIsImFkbWluOnBhcmtpbmciLCJhZG1pbjpzcGlyaXRmb25kIiwicmVhZDphY2NvdW50aW5nIiwiYWRtaW46Y2FsZW5kYXIiLCJyZWFkOlN1YkNvbnRyYWN0b3JSZXBvcnQiLCJhZG1pbjpiZWtrYm9rIiwiYWRtaW46YXRsYXMiLCJhZG1pbjpzdGFmZmluZyJdLCJodHRwczovL2FwaS5iZWtrLm5vL2NsYWltcy9lbXBsb3llZUlkIjoxNDM3LCJuYW1lIjoiQmrDuHJuLUl2YXIgU3Ryw7htIiwiaXNzIjoiaHR0cHM6Ly9iZWtrLWRldi5ldS5hdXRoMC5jb20vIiwic3ViIjoid2FhZHxMT0FIbFBSbEJEd2JMdnhSVHdjdVNOY3FfZFkwZlBpNnJaWk1VREJTYTB3IiwiYXVkIjoiUUhReTc1Uzd0bW5oRGRCR1lTbnN6emxoTVB1bDBmQUUiLCJpYXQiOjE2OTMyODk5ODgsImV4cCI6MTY5MzMyNTk4OH0.FeEhWeBLeyimOiGVE3qCWCFXwQH2EQprKlV-ePPPNMqMyhTk2K0_4NdPXI0_WRjoo_J_0c_s37Rf9zbLUrMVqZs5e9AorO39NSyQqrYjR6uFVqooOrWYw7G-SdIsOaZ_lxAToeNOgcB5pHSwLoyFF04aJi2K4-mpomQxkZuNESYEvkr0QbGLXebL-51hqQ_vr-lKbcbugsqHcK42ta-pemBkC0aNjfSuQxtPbLvn3iKMXB6vrdXCoYpEgNrtqiLG8EdJHPhWFdqe70QIXi6e9qu3FuDYEc_G6xaaf0FPje2Kv1wtInU95gtD6UeP9m_lINUdvdCSiKpH2-HzkqMHgQ";
-  const eventId = "3e2e0d14-5656-4e56-88ec-fa324e8c397a";
+    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik56RTVOVFJHUVRnNVJVRkNSVFJEUkRnelEwUXdORE5GUkRZeU4wWkZNVEJGUmpCRFFUVkJRUSJ9.eyJodHRwczovL2FwaS5iZWtrLm5vL2NsYWltcy9wZXJtaXNzaW9uIjpbInJlYWQ6c3RhZmZpbmciLCJ3cml0ZTpzdGFmZmluZyIsIndyaXRlOnRlbnRhdGl2U3RhZmZpbmciLCJyZWFkOnRlbnRhdGl2U3RhZmZpbmciLCJyZWFkOmVtcGxveWVlcyIsInJlYWQ6YmVrayIsIndyaXRlOmVtcGxveWVlcyIsIndyaXRlOnRpbWVjb2RlcyIsIndyaXRlOmFjY291bnRpbmciLCJhZG1pbjppbnZvaWNlLXN2YyIsImFkbWluOlNhbGVzQW5kUHJvamVjdHMtc3ZjIiwicmVhZDpvcHBvcnR1bml0eSIsIndyaXRlOm9wcG9ydHVuaXR5Iiwid3JpdGU6cHJvamVjdCIsInJlYWRXcml0ZTpwcm9nbm9zaXMiLCJyZWFkV3JpdGU6c3ViY29udHJhY3RvciIsImFkbWluOnRpbWVrZWVwZXItc3ZjIiwicmVhZDplZ2VubWVsZGluZ2VyIiwicmVhZDp0aW1lY29kZXMiLCJyZWFkV3JpdGU6dGltZXNoZWV0cyIsInJlYWQ6aW52b2ljZXMiLCJyZWFkOmN1c3RvbWVyIiwicmVhZDpwcm9qZWN0IiwicmVhZFdyaXRlOmNhbGVuZGFyIiwiYmF0Y2hVcGRhdGU6Y2FsZW5kYXIiLCJyZWFkV3JpdGU6cGFya2luZyIsInJlYWRXcml0ZTpldmVudHMiLCJ3cml0ZTpwcmFjdGljZUdyb3VwcyIsInJlYWQ6Y3YiLCJkZWxldGU6YXV0aDB1c2VyIiwiYWRtaW46Y2FiaW4iLCJyZWFkOmZvcnNpZGUiLCJyZWFkOmNhYmluIiwiYWRtaW46YXV0aCIsImFkbWluOmVtcGxveWVlLXN2YyIsImFkbWluOmFycmFuZ2VtZW50IiwicmVhZDphcnJhbmdlbWVudCIsImFkbWluOnBhcmtpbmciLCJhZG1pbjpzcGlyaXRmb25kIiwicmVhZDphY2NvdW50aW5nIiwiYWRtaW46Y2FsZW5kYXIiLCJyZWFkOlN1YkNvbnRyYWN0b3JSZXBvcnQiLCJhZG1pbjpiZWtrYm9rIiwiYWRtaW46YXRsYXMiLCJhZG1pbjpzdGFmZmluZyJdLCJodHRwczovL2FwaS5iZWtrLm5vL2NsYWltcy9lbXBsb3llZUlkIjoxNDM3LCJuYW1lIjoiQmrDuHJuLUl2YXIgU3Ryw7htIiwiZW1haWwiOiJiam9ybi5pdmFyLnN0cm9tQGJla2subm8iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6Ly9iZWtrLWRldi5ldS5hdXRoMC5jb20vIiwic3ViIjoid2FhZHxMT0FIbFBSbEJEd2JMdnhSVHdjdVNOY3FfZFkwZlBpNnJaWk1VREJTYTB3IiwiYXVkIjoiUUhReTc1Uzd0bW5oRGRCR1lTbnN6emxoTVB1bDBmQUUiLCJpYXQiOjE2OTMzNzYxODksImV4cCI6MTY5MzQxMjE4OX0.BZM5TedEHCpxrz8cjTeseAhgpE4M5hBUNRiC6bxUghUvMxreUqpJ5FnUlWDtXs9Xy6YQk_nGfR-DW82BJ1GJwGYhMBUeUX3xo2JaqobfdvhpXeuWa9fkB8fZwFsAzlaJQpHqiBJQ98tJIJfm0tcugK9JgkKrdI37pEk94WqwIpz0R8b53hsYFnOgeRFwzjPgyNQbOgchvfm5uU4UjCsPcJ_sbvkb0hNU63MSeVC1j3MayaW8QVQ_KbuMH9XDtkswlPKkzdV9hpJ9lTLIUDO0MsgqcQMEzrol1VoWUwRCfZekFQek0kt2Tg4aW431sYXADDxEu5A1hsXHsP4TC0nz0A";
+  const eventId = "aa42634a-cc08-4446-b7d3-2c0489afe561";
   let email = `${randomString(10)}@foo`;
   const params = {
     headers: {
@@ -33,38 +33,38 @@ export default function () {
     // participantAnswers: ["1", "2", "3", "4", "5"],
       participantAnswers: [
       {
-        questionId: "1",
+        questionId: "2572",
         eventId,
         email,
-        question: "Spørsmål 1",
+        question: "spm one",
         answer: "1",
       },
       {
-        questionId: "2",
+        questionId: "2573",
         eventId,
         email,
-        question: "Spørsmål 2",
+        question: "spm two",
         answer: "2",
       },
       {
-        questionId: "3",
+        questionId: "2574",
         eventId,
         email,
-        question: "Spørsmål 3",
+        question: "spm three",
         answer: "3",
       },
       {
-        questionId: "4",
+        questionId: "2575",
         eventId,
         email,
-        question: "Spørsmål 4",
+        question: "spm four",
         answer: "4",
       },
       {
-        questionId: "5",
+        questionId: "2576",
         eventId,
         email,
-        question: "Spørsmål 4",
+        question: "spm five",
         answer: "4",
       },
     ],
